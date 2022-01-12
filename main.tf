7c836761467391f8cb5385b612b615a2159b9705
jasonwalsh <jasonwalsh@tutanota.com>
2022-01-12 17:11:29 -0500

    feat: add default topics to repository

--- main.tf
+++ main.tf
@@ -40,4 +40,5 @@ resource "github_repository" "this" {
   has_projects           = false
   has_wiki               = false
   name                   = each.key
+  topics                 = concat(lookup(each.value, "topics", []), ["terraform-managed"])
 }
